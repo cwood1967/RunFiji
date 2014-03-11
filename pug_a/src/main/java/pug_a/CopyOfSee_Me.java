48839fe7f8dfdc0bf427527a180af85a3ffc57a1
Chris Wood <cjw@stowers.org>
2014-03-11 13:46:07 -0500

    minor change to test

--- pug_a/src/main/java/pug_a/CopyOfSee_Me.java
+++ pug_a/src/main/java/pug_a/CopyOfSee_Me.java
@@ -8,6 +8,7 @@ import ij.ImageJ;
 public class CopyOfSee_Me  implements PlugIn {
 	public void run(String arg) {
 		IJ.log("hello, this is a copy");
+		IJ.log("hello, this is a copy");
 		
 		ImagePlus image = IJ.openImage("http://imagej.net/images/clown.jpg");
 		image.show();
