dc4108ac880ce363e8df0736bc6862a732204849
cjw <cjw@stowers.org>
2015-06-08 15:08:52 -0500

    after more than a year

--- sliceMean/src/main/java/SliceMean/Head.java
+++ sliceMean/src/main/java/SliceMean/Head.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ForkJoinPool;
+//import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
 
 import ij.IJ;
